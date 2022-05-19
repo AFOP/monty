da1bfe72c61c9ccec23c678d132f0c4654379a11
AFOP <4592@holbertonschool.com>
2022-05-19 10:56:47 -0500

    Update Task 0

--- main.c
+++ main.c
@@ -11,12 +11,12 @@ int main(int argc, char **argv)
 	char *line_c;
 	size_t size_l;
 	ssize_t status;
-	stack_t *h;
+	stack_t *h = NULL;
 	FILE *fp;
 
 	if (argc != 2)
 	{
-		printf("USAGE: %s file\n", argv[0]);
+		printf("Usage: %s file\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 	fp = fopen(argv[1], "r");
@@ -29,9 +29,11 @@ int main(int argc, char **argv)
 		line_c = NULL;
 		size_l = 0;
 		status = getline(&line_c, &size_l, fp);
-		line_number++;
 		if (status > 0)
+		{
+			line_number++;
 			execute(&h, line_c, line_number);
+		}
 		else
 			free(line_c);
 	} while (status >= 0);
