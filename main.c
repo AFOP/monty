7d94db1af7e7d31b2488391de28076f2193353f9
AFOP <4592@holbertonschool.com>
2022-05-22 21:19:42 -0500

    Update

--- main.c
+++ main.c
@@ -14,13 +14,14 @@ int main(int argc, char **argv)
 	stack_t *h = NULL;
 	FILE *fp = NULL;
 
-	if (argc != 2 || getsubopt(argv, ":m") == -1)
+	if (argc != 2 || strcspn(argv[1],".m") == 0)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+	
 	fp = fopen(argv[1], "r");
-	if (fp == NULL)
+	if (!fp)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
